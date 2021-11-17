384fa3b368519c2048bdeb1e308ce34e78bb04cc
janfritsvegter <jan.frits.vegter@gmail.com>
2021-11-17 14:16:47 +0100

    first commit (main)

---
+++ src/main/java/jfv/marah/springbootcursus/SpringbootCursusApplication.java
@@ -0,0 +1,13 @@
+package jfv.marah.springbootcursus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCursusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCursusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jfv/marah/springbootcursus/SpringbootCursusApplicationTests.java
@@ -0,0 +1,13 @@
+package jfv.marah.springbootcursus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCursusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
